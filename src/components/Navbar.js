5c5fe5e3d23ccf102c2a65949b9e4e76ecd7e087
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2019-01-24 08:11:42 -0300

    Navbar Structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
-
-class componentName extends Component {
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg'
+export default class Navbar extends Component {
     render () {
         return (
-            <div>
-                <h3>Hello from navbar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm nabvbar-dark bg-primary px-sm-5">
+                <Link to='/'>
+                    <img className="navbar-brand" src={logo} alt="store"/>
+                </Link>
+                <ul className="navbar-nav aligh-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to='/' className="nav-link">
+                            Products 
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus"/>>
+                            My Cart
+                    </button>
+                </Link>
+            </nav>
         )
     }
 }
 
-export default componentName
+
+
